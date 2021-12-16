01a287b9190db510153a71282222fe77e3650b6a
Bruno Santos <Bruno Santos@BRSAN7>
2021-12-16 11:08:09 -0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = "World!";
+            var c = " ";
+            var d = 7.5;
+            var e = 5.5;
+            var f = (d - e) + c + a + b;
+            Console.WriteLine("Hello World!\n");
+            Console.WriteLine(f);
+            Console.WriteLine("\n\nTestando\n");
+            for (var i =1; i<=3; ++i)
+            {
+                Console.WriteLine(i + "\n");
+            }
+            Console.WriteLine("Testando");
+        }
+    }
+}
